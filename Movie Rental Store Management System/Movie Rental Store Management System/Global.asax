4a4a57cc6a0311e075c4b559a9f9ae39693b34b5
ankitkhanna.khanna90 <ankitkhanna.khanna90@gmail.com>
2017-03-14 19:09:15 +0530

    First Commit

---
+++ Movie Rental Store Management System/Movie Rental Store Management System/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Movie_Rental_Store_Management_System.MvcApplication" Language="C#" %>
---
+++ Movie Rental Store Management System/Movie Rental Store Management System/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Movie_Rental_Store_Management_System.Startup))]
+namespace Movie_Rental_Store_Management_System
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
